0c839e64245c4570e0de0e11ed890c8ee689149d
Adarshcodes <adarsh.lrnr@gmail.com>
2022-01-29 13:54:44 +0530

    created card using props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,36 @@
 import React from "react";
-import UserImg from "../images/katie-zaferes.png";
 import StarImg from "../images/star.png";
 
 class Card extends React.Component {
 	render() {
 		return (
-			<div className="card-container">
-				<div className="card">
-					<div className="card--img-container">
-						<img
-							src={UserImg}
-							alt="user"
-							className="card--img-container__img"
-						/>
-						<div className="card--img-container__status">Sold Out</div>
-					</div>
+			<div className="card">
+				<div className="card--img-container">
+					<img
+						src={this.props.img}
+						alt="user"
+						className="card--img-container__img"
+					/>
+					<div className="card--img-container__status">{this.props.status}</div>
+				</div>
 
-					<div className="card--details">
-						<div className="card--details__rating">
-							<img src={StarImg} alt="star" className="card--details__star" />
-							<p className="card--details__rating-score">5.0</p>
-							<p className="card--details__user-count">(6)</p>
-							<p className="card--details__user-country">&#xb7; USA</p>
-						</div>
-						<h4 className="card--details__info">
-							Life lessons with Katie Zaferes
-						</h4>
-						<p className="card--details__pricing">
-							<b>From $136 /</b> person
+				<div className="card--details">
+					<div className="card--details__rating">
+						<img src={StarImg} alt="star" className="card--details__star" />
+						<p className="card--details__rating-score">
+							{this.props.ratingScore}
+						</p>
+						<p className="card--details__user-count">
+							({this.props.userCount})
+						</p>
+						<p className="card--details__user-country">
+							&#xb7; {this.props.userCountry}
 						</p>
 					</div>
+					<h4 className="card--details__info">{this.props.info}</h4>
+					<p className="card--details__pricing">
+						<b>From {this.props.pricing} /</b> person
+					</p>
 				</div>
 			</div>
 		);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,45 @@ import NavBar from "./NavBar";
 import Hero from "./Hero";
 import Card from "./Card";
 
+// importing images
+import UserImg from "../images/katie-zaferes.png";
+
 export default class Header extends React.Component {
 	render() {
 		return (
 			<header className="header">
 				<NavBar />
 				{/* <Hero /> */}
-				<Card />
+				<div className="card-container">
+					<Card
+						img={UserImg}
+						status="Sold out"
+						ratingScore="5.0"
+						userCount="6"
+						userCountry="USA"
+						info="Life lessons with Katie Zaferes"
+						pricing="$136"
+					/>
+					<Card
+						img={UserImg}
+						status="Sold out"
+						ratingScore="5.0"
+						userCount="6"
+						userCountry="USA"
+						info="Life lessons with Katie Zaferes"
+						pricing="$136"
+					/>
+					<Card
+						img={UserImg}
+						status="Sold out"
+						ratingScore="5.0"
+						userCount="6"
+						userCountry="USA"
+						info="Life lessons with Katie Zaferes"
+						pricing="$136"
+					/>
+				</div>
+				;
 			</header>
 		);
 	}
